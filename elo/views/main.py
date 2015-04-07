704828f63baff672e372f7cd8d65e3cfa3d87edc
张鹏 <zhangpeng1@infohold.com.cn>
2015-04-07 18:02:41 +0800

    修改配置文件

--- elo/views/main.py
+++ elo/views/main.py
@@ -24,20 +24,28 @@ class PartyList(Handler):
         p = self.args
         p["event_point"] = "1000"
 
+        headers = self.request.headers
+        del headers["Proxy-Connection"]
+        del headers["Connection"]
+        del headers["X-Scheme"]
+        del headers["X-Real-IP"]
+        print headers
         post_url = urlparse.urljoin(CONFIG("DOMAIN"), self.reverse_url(self.kwargs["name"]))
         logger.info(post_url)
         #response = requests.post(post_url, data=params, headers=self.request.headers)
         params = urllib.urlencode(p)
         logger.info("CALLBACK_REQ_DATA: %s" % params)
-        req = urllib2.Request(url=post_url, data=params)
+        req = urllib2.Request(url=post_url, data=params, headers=headers)
+        from code import interact
+        #interact(local=locals())
         logger.info("*****CALLBACK_REQ_ALL: %s" % str(post_url + "?" + params))
         req_start = time.time()
         response = urllib2.urlopen(req).read()
         logger.info("*****callback response: %s" % response)
-        response = response.decode('utf-8')
+        #response = response.decode('utf-8')
         req_end = time.time()
         logger.info("CALLBACK_REQ_TIME: START:[%s]_____END:[%s]_____USE:[%s]" % (req_start, req_end, req_end - req_start))
         logger.info("CALLBACK_RESPONSE: %s" % response)
 
         logger.info(response)
-        self.write("success")
+        self.write(response)
